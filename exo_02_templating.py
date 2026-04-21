e34510dcf35d8ec3c6929dbe3dc3b70e6f720eaa
mlamamra <mlamamra@dawan.fr>
2026-04-21 14:50:11 +0200

    fonction: usage de **

--- exo_02_templating.py
+++ exo_02_templating.py
@@ -42,18 +42,27 @@ technique
 5/ tester dans différents cas
 """
 
+DEBUG = True
 
-def parse_template(tpl: str, data: dict, delims: tuple=("{{", "}}"), default="N/A") -> str:
+def parse_template(
+    tpl: str, 
+    data: dict, 
+    delims: tuple=("{{", "}}"), 
+    default="N/A",
+    **opts
+) -> str:
   while delims[0] in tpl:
     index_start = tpl.find(delims[0]) + len(delims[0])
     index_end = tpl.find(delims[1])
     key = tpl[index_start:index_end]
+    if "debug" in opts and opts["debug"]:
+        print(f"key trouvée: {key}")
     tpl = tpl.replace(delims[0] + key + delims[1], str(data.get(key, default)))
 
   return tpl
 
 print(parse_template(_template, injections, delims=("((", "))")))
 
-print(parse_template("blabla {{value}}", {"value": 50}))
+print(parse_template("blabla {{value}}", {"value": 50}, debug=DEBUG))
 
 # %%
