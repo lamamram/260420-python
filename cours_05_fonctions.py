130a0bd8770a6fda286f3023ca7c07c8cb124747
mlamamra <mlamamra@dawan.fr>
2026-04-21 12:03:26 +0200

    fonctions 1

--- cours_05_fonctions.py
+++ cours_05_fonctions.py
@@ -4,7 +4,8 @@
 ###     et après un bloc d'instructions  
 
 # définition d'une fonction ma_fonction() qui contient l'affichage de "coucou"
-
+def ma_fonction():
+    print("coucou")
 
 ### pour exécuter le bloc de la définition, on doit appeler la fonction 
 ### <nom_de_la_fonction>()
@@ -12,30 +13,75 @@
 ### une fonction est un type de donnée dite "callable"
 
 # afficher l'appel de la fonction ma_fonction
-
+print("appel de ma_fonction() => ", ma_fonction())
 # %% ----------- retour d'une fonction ------------------------------
 
 # même définition mais en remplaçant le print("coucou") par le mot clé return et 'coucou'
-
+def ma_fonction():
+  return "coucou"
+  x = 5 # cette ligne ne sera jamais exécutée car elle est après le return
 
 # afficher l'appel de la fonction
-
+print("appel de ma_fonction() => ", ma_fonction())
 # alternativement, on peut affecter l'appel de la fonction dans une variable
-
+ret = ma_fonction()
 # %% -- définition d'une fonction avec des paramètres et appel des paramètres ---
 
 # définir une fonction qui ajoute 2 entiers en paramètres et qui retourne la somme 
+# a, b dans la définition de la focntions sont des paramètres formels
+def addition(a, b):
+    c = 2 # c est une variable locale à la fonction
+    return a + b + c
 
-
+a, b = 5, 10 # a et b sont des variables globaes du programme
+# injection des variables globales en tant que paramètres d'appel: passage en référence
+print(f"appel de addition({a}, {b}) => ", addition(a, b))
+# print(f"appel de addition("ff", "rr") => ", addition("ff", "tt")) ERROR
 # %% ------------ annotations, documentation et contrôle -------------------
 
 
 # créer une fonction division de 2 floats qui retourne un float
+def division(a: float, b: float) -> float:
+       """cette fonction retourne le résultat de la division de a par b"""
+       if b == 0:
+              return "division par zéro impossible"
+       return a / b
+
+# les annotations ne sont pas des contrôles, ce ne sont que des indications
+def division(a: float, b: float) -> tuple:
+       """DOCSTRING: cette fonction retourne le résultat de la division de a par b"""
+       retour, erreur = 0, ""
+       if b == 0:
+              erreur = "division par zéro impossible"
+       else:
+             retour = a / b
+       return retour, erreur
+
+ret, err = division(5, 0)
+print(f"résultat: {ret} et erreur: {err}")
 # 1. en notifiant les types d'entrée et de sortie de la fonction
 # 2. documenter la fonction en ajoutant une """doctstring""" au début du bloc
 # 3. faire un contrôle sur le dénominateur
 
+# %% --------------- passage en référence de types mutables et immutables ------------------
 
+def test_immmutable(x: int):
+      x += 1
+      print(x, id(x))
+      return x
+
+x = 5
+test_immmutable(x)
+print(x, id(x)) # x global n' a pas changé car x est immutable => le x local ne peut pas être référencé à x global
+
+def immmutable(l: list, elem: int):
+      l.append(elem)
+      print(l, id(l))
+      return l
+
+l = [1, 2, 3]
+immmutable(l, 4)
+print(l, id(l)) # l global a changé car l est mutable => le l local
 
 # %% --------------- types de paramètres ----------------------------------
 
