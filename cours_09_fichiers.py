95fcffb7e9527069dcfd7d8497548b2fdd21e400
mlamamra <mlamamra@dawan.fr>
2026-04-23 12:11:02 +0200

    fichier

--- cours_09_fichiers.py
+++ cours_09_fichiers.py
@@ -8,49 +8,76 @@
 
 # creér (ouvrir en mode création) le fichier "mon_fichier.txt" en encodage utf-8
 
+f = open("./mon_fic.txt", "w", encoding="utf-8")
+
+
 # écriture de deux lignes avec les sauts de lignes
-"1ère ligne\n"
-"2ème ligne\n"
+f.write("1ère ligne\n")
+f.write("2ème ligne\n")
+
 
 # important! il faut fermer ce qu'on a ouvert
-
+f.close()
 
 # %% ----------------- lire le contenu d'un fichier ---------------------
 
 # ouvrir le fichier en lecture et même encodage
+f = open("./mon_fic.txt", "r", encoding="utf-8")
 
 # lire une ligne (avec \n)
+# print rajoute un 2ème  \n
+print(f.read(11))
 
 # lire tout
-
+print(f.read())
 
 ## REM. notion de curseur: la deuxième lecture reprend 
 ## depuis la fin de la première ligne
-
+f.close()
 
 # %% ------------- écriture à la fin du fichier (append) -------------------
-
+f = open("./mon_fic.txt", "a", encoding="utf-8")
 # écrire une 3ème ligne dans le fichier 
+f.write("3ème ligne\n")
 
 
 # vérifier que le contenu existant n'a pas été supprimé
+f.close()
 
+# %% --------------------- readlines / writlelines --------------
+f = open("./mon_fic.txt", "r", encoding="utf-8")
+print(f.readlines(11))
+f.close()
+
+f = open("./mon_fic.txt", "a", encoding="utf-8")
+f.writelines(["4ème ligne\n", "5ème ligne\n"])
+f.close()
 
 # %% ------------------- modes avancés ------------------------------
 # remplacer la ligne n
-
+f = open("./mon_fic.txt", "r+", encoding="utf-8")
 # r+: on a un curseur au début en lecture
 #   : on a également un curseur à la fin en écriture (a priori)
 #   : on peut replacer le curseur d'écriture
 
 n = 3
 
+# la lecture après sera en erreur car le curseur est positionné sur la moitué d'un caractère
+# f.seek(2) # UnicideDecodeError
+
 # je lis 1 ligne du fichier pour positionner le curseur de lecture
-
-
+for _ in range(n- 1):
+  f.readline()
 
 ## rem f.tell() : position du curseur d'écriture/lecture
+# la 1ère ligne est composé de 11 caractère
+# tell() retourne est nb d'octet lié à l'encodage => 13
+# utf-8 : 1 octer pour les caractère ASCII et 2 octets avec les autres (è, \n)
+
+pos = f.tell()
 ## f.seek(pos) : positionne les curseurs d'écriture/lecture à la position pos
+f.seek(pos)
+f.write("bonjour\n")
 ## attention : read et write utilise des caractère 
 ##           : alors que tell et seek utilise des octets !!!
 
@@ -59,9 +86,9 @@ n = 3
 # écrire un nouveau contenu + \n
 
 # supprimer le reste du fichier après le curseur
-
+f.truncate()
 # je me repositionne au début du fichier pour lire le contenu
-
+f.close()
 
 # %% -- faciliter les ouvertures/fermetures: gestionnaires de contexte: with --
 
@@ -69,12 +96,16 @@ n = 3
   # fichier ouvert
   # ....
 
+with open("./mon_fic.txt", "r", encoding="utf-8") as f:
+  print(f.read())
+
+
 # ici fichier fermé
 
 
 # %% ----------- un fichier est un itérable de lignes ----------------------
 
-with open("./mon_fichier.txt", mode="r", encoding="utf-8") as f:
+with open("./mon_fic.txt", mode="r", encoding="utf-8") as f:
   for line in f:
     print(line)
 
@@ -82,8 +113,15 @@ with open("./mon_fichier.txt", mode="r", encoding="utf-8") as f:
 
 n = 2
 
+l = []
 # lire toutes les lignes du fichier dans une liste sauf la ligne à supprimer
+with open("./mon_fic.txt", mode="r", encoding="utf-8") as f:
+  for i, line in enumerate(f, start=1):
+    if i != n:
+      l.append(line)
 
+with open("./mon_fic.txt", mode="w", encoding="utf-8") as f:
+  f.write("".join(l))
 # écraser le fichier avec les lignes de liste
 
 # %% -----------------  création d'un csv -----------------------------
