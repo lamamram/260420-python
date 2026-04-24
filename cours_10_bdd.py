64d754367b90d08a99e800fa9e91096c11de57db
mlamamra <mlamamra@dawan.fr>
2026-04-24 11:58:36 +0200

    bdd: exemple vanilla

--- cours_10_bdd.py
+++ cours_10_bdd.py
@@ -1,4 +1,32 @@
 # %% ---------------- code impératif avec sqlite3 -------------
+"""
+SQLITE3: BDD relationnelle avec un dialecte SQL simple
+BDD en un seul fichier ou chargé en RAM
+pas de host / port / user / password / droits
+le module sqlite3 python est dans la lib standard
+
+sqlite3 utilise la DbAPI python pour tous les clients de BDD en python
+- méthode connect() => connexion (création ou connexion sur la bdd)
+- méthode cursor()  => prompt sur la connexion
+- méthode execute() => exécuter une requête SQL sur le cursor
+- méthode fetch()   => faire sortir les enregistrements attendus
+- méthode close()   => fermer la cnx
+---------------- gérer les transactions -----------------
+- méthode begin()   => commence une transaction
+- methode commit()  => confitmer une tr
+- méthode rollback()=> annuler une tr
+"""
+
+import sqlite3
+
+conn = sqlite3.connect("dns.db")
+cur  = conn.cursor()
+req  = cur.execute("SELECT SQLITE_VERSION()")
+row  = cur.fetchone()
+print(row)
+conn.close()
+
+# %% ----------------- une connexion s'ouvre et se ferme ------------------ 
 
 
 
